19cfff0312a113172e60e5cff69e565105bc8f5e
Joann Pan <joannlpan@gmail.com>
2020-05-01 16:37:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,20 @@
 # Write your code here.
-
+require 'pry'
 katz_deli = []
 
 def line(katz_deli)
  #shows everyone their current place in the line.
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else 
+    katz_deli.each_with_index do |customer, index|
+      line = []
+      line << "#{index + 1}. #{customer}"
+      binding.pry
+      puts "The line is currently: #{index + 1}. #{customer}"
 
-  #if no one
-  #  "The line is currently empty."
-
+      
+  end
 end
 
 
