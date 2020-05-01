87e7a46ea214cd2a63a48c7b48d326c86f4f13be
Joann Pan <joannlpan@gmail.com>
2020-05-01 18:05:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ def take_a_number(katz_deli, new_customer)
 end
 
 def now_serving(katz_deli)
-   if katz.deli.size == 0
+   if katz_deli.size == 0
      puts "There is nobody waiting to be served!"
    else "Currently serving: #{katz_deli.first}"
      katz_deli.shift
