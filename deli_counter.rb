82ff4fe4642c4b1041d445b570fcf2b78a69c9f3
Joann Pan <joannlpan@gmail.com>
2020-05-01 16:52:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |customer, index|
-      binding.pry
+      
       line = []
       line << "#{index + 1}. #{customer}"
       line.join(", ")
