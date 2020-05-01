2715df9eca49c188c08d6c664eb656de81012d55
Joann Pan <joannlpan@gmail.com>
2020-05-01 16:21:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ end
 def take_a_number(katz_deli, new_customer)
   katz_deli << new_customer
   katz_deli.each_with_index do |customer_in_line, index|
-    puts "Welcome, #{customer_in_line}. You are number #{index + 1} in line.
+    puts "Welcome, #{customer_in_line}. You are number #{index + 1} in line."
   end
   end
 
